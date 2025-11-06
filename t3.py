075db8334f6bf293c618e2a66ed6629b4faf9d13
Daniel Patterson <102685674+dpatt27@users.noreply.github.com>
2025-11-06 11:30:56 -0800

    Add files via upload

---
+++ t3.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+st.write("hello")
+st.title("My first app")
+
+df = pd.DataFrame(np.random.randn(10,2), columns= ["Product A", "Product B"])
+st.line_chart(df)
+st.bar_chart(df)
+
+df2 = pd.DataFrame(np.random.randn(500,2)/[50, 50] + [30.5, -97.6], columns=["lat", "lon"])
+st.map(df2)
+
+
+st.video("/Users/danielpatterson/Downloads/#8 GRAND SLAM.mp4")
+
+st.checkbox("this is a checkbox")
+st.button("Select")
+st.radio("Choose one", ["option 1", "option 2", "option 3"])
+st.multiselect("multiselect", ["option 1", "option 2", "option 3"])
+st.slider("slider", 0, 100, 50)
+
+st.header("Header")
+st.latex(r"E=mc^2")
+
+
+data = np.random.normal(loc=1, scale=2, size=5000)
+
+fig, ax = plt.subplots()
+ax.hist(data, bins=15)
+ax.set_xlabel("Value")
+ax.set_ylabel("Frequency")
+
+st.pyplot(fig)
